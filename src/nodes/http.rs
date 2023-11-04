6f1962ca35b97f45823b1feb870d342acf141a34
Simon Riepl <s.riepl1@googlemail.com>
2023-11-04 22:06:39 +0100

    requests possible with defined json scheme, body is returned, basic tests with mockito added

--- src/nodes/http.rs
+++ src/nodes/http.rs
@@ -1,11 +1,16 @@
-use flowrs::node::SendError;
+use std::collections::HashMap;
+use std::str::FromStr;
+
 use flowrs::RuntimeConnectable;
 use flowrs::{
     connection::{Input, Output},
     node::{ChangeObserver, Node, UpdateError},
 };
+use reqwest::Method;
 use reqwest::blocking::Client;
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(RuntimeConnectable, Deserialize, Serialize)]
 
@@ -26,24 +31,18 @@ Config Input
     "timeout": 10000
 }
 */
-pub struct HttpNode<I>
-where
-    I: Clone,
-{
+pub struct HttpNode {
     #[input]
-    pub data_input: Input<I>,
+    pub data_input: Input<serde_json::Value>,
 
     #[input]
-    pub config_input: Input<I>,
+    pub config_input: Input<serde_json::Value>,
 
     #[output]
     pub output: Output<String>,
 }
 
-impl<I> HttpNode<I>
-where
-    I: Clone,
-{
+impl HttpNode {
     pub fn new(change_observer: Option<&ChangeObserver>) -> Self {
         Self {
             data_input: Input::new(),
@@ -53,30 +52,78 @@ where
     }
 }
 
-impl<I> Node for HttpNode<I>
-where
-    I: Clone + Send,
-{
+fn extract_key_value_pairs(json_value: &Value) -> HeaderMap{
+    //let mut headers = HashMap::new();
+    let mut header_map = reqwest::header::HeaderMap::new();
+
+
+    if let Value::Object(raw_header_map) = json_value {
+        for (key, value) in raw_header_map {
+            //if let Value::String(header_value) = value {
+            //    headers.insert(key.to_string(), header_value.to_string());
+            //}
+            let header_name = HeaderName::from_str(&key.to_lowercase());
+            if header_name.is_err() {
+                continue;
+            }
+        
+            let value_str = &value.as_str().unwrap_or("").to_lowercase();
+            let header_value = HeaderValue::from_str(&value_str).unwrap_or(HeaderValue::from_static(""));
+            header_map.insert(header_name.unwrap(), header_value);
+            }
+    }
+
+    // headers
+    header_map
+}
+
+impl Node for HttpNode {
     fn on_update(&mut self) -> Result<(), UpdateError> {
-        let Ok(input) = self.data_input.next() else {
-            return Err(UpdateError::Other(anyhow::Error::msg(
-                "No valid data input.",
-            )));
-        };
+        let input = self.data_input.next()?;
 
-        let client = Client::new();
+        let mut client_builder = Client::builder();
 
-        let response = client
-            .get("https://catfact.ninja/fact")
-            //.header(AUTHORIZATION, "Bearer [AUTH_TOKEN]")
-            //.header(CONTENT_TYPE, "application/json")
-            //.header(ACCEPT, "application/json")
-            .send();
+        let method_str = match &input["method"] {
+            Value::String(m) => Ok(m.clone()),
+            _ => Err(UpdateError::Other(anyhow::Error::msg(
+                "No REST method given. Key \"method\" with a valid REST verb as value is required. Currently supported: GET.",
+            ))),
+        }?;
+
+        let url = match &input["url"] {
+            Value::String(u) => Ok(u.clone()),
+            _ => Err(UpdateError::Other(anyhow::Error::msg(
+                "No URL given.",
+            ))),
+        }?;
+
+        let headers = extract_key_value_pairs(&input.get("headers").unwrap_or(&Value::Null));
+        
+        client_builder = client_builder.default_headers(headers);
+
+        let body_value = input.get("body").unwrap_or(&Value::Null);
+        let body = serde_json::to_string(&body_value).unwrap_or(String::from(""));
+
+        let reqwest_err_map = |e: reqwest::Error| UpdateError::Other(anyhow::Error::msg(e.to_string()));
+        
+        let built_client = client_builder.build().map_err(reqwest_err_map)?;
+
+        let method = match method_str.to_lowercase().as_str() {
+            "get" => Ok(Method::GET),
+            "post" => Ok(Method::POST),
+            "put" => Ok(Method::PUT), // not tested
+            "delete" => Ok(Method::DELETE), // not tested
+            other => Err(UpdateError::Other(anyhow::Error::msg(
+                format!("HTTP request method \"{other}\" not implemented or not valid."),
+            ))),
+        }?;
+
+        let response = built_client.request(method, url).body(body).send();
 
         match response {
             Ok(response) => {
                 let body = response.text().unwrap();
-                let _ = self.output.clone().send(body.clone());
+                self.output.send(body)?; // TODO: return body + headers
                 Ok(())
             }
             Err(e) => Err(UpdateError::Other(anyhow::Error::msg(e.to_string()))),
--- tests/nodes/test_http.rs
+++ tests/nodes/test_http.rs
@@ -3,22 +3,169 @@ mod nodes {
 
     use flowrs::{
         connection::{connect, Edge},
-        node::{ChangeObserver, Node, ReceiveError},
+        node::{ChangeObserver, Node},
     };
     use flowrs_std::http::HttpNode;
-    use flowrs_std::value::ValueNode;
+    use serde_json::json;
+
 
     #[test]
-    fn receive_string_from_value_node() {
+    fn get_request() {
+        let mut server = mockito::Server::new();
+
+        let url = server.url();
+        let method = "GET";
+        let path = "/hello&question=how%20are%20you";
+        let expected_response_body = "Hello World!";
+
+        // Create a mock
+        let mock = server
+            .mock(&method, path.clone())
+            .with_status(200)
+            .with_body(expected_response_body)
+            .create();
+
         let change_observer: ChangeObserver = ChangeObserver::new();
-        let value_node = ValueNode::new(String::from("Hello, world!"), Some(&change_observer));
-        let mut http_node: HttpNode<String> = HttpNode::new(Some(&change_observer));
+        let data_input_json = json!({
+        "url": url.to_string() + path,
+        "method": method
+        });
+     
+        let mut http_node: HttpNode = HttpNode::new(Some(&change_observer));
         let mock_output = Edge::new();
         connect(http_node.output.clone(), mock_output.clone());
-        connect(value_node.output.clone(), http_node.data_input.clone());
-        value_node.on_ready().unwrap();
+        let _ = http_node.data_input.send(data_input_json.clone());
         http_node.on_update().unwrap();
 
-        mock_output.next().unwrap();
+        mock.assert(); // checks if the mock server has been called
+        let returned_body = mock_output.next().unwrap();
+        assert!(returned_body == expected_response_body, "expected_body: {}, returned_body: {}", expected_response_body, returned_body);
+    }
+
+    #[test]
+    fn get_request_with_headers_and_body() {
+        let mut server = mockito::Server::new();
+
+        let url = server.url();
+        let method = "GET";
+        let path = "/hello";
+        let content_type_header = ("content-type", "text/plain");
+        let x_api_key_header = ("x-api-key", "1234");
+        let expected_response_body = "AI";
+
+        // Create a mock
+        let mock = server
+            .mock(&method, path.clone())
+            .with_status(200)
+            .match_header(&content_type_header.0, content_type_header.1)
+            .match_header(&x_api_key_header.0, x_api_key_header.1)
+            .with_body(&expected_response_body)
+            .create();
+
+
+        let change_observer: ChangeObserver = ChangeObserver::new();
+        let data_input_json = json!({
+        "url": url.to_string() + path,
+        "method": method,
+        "headers": {
+            content_type_header.0: content_type_header.1,
+            x_api_key_header.0: x_api_key_header.1
+        },
+        "body": expected_response_body
+        });
+     
+        let mut http_node: HttpNode = HttpNode::new(Some(&change_observer));
+        let mock_output = Edge::new();
+        connect(http_node.output.clone(), mock_output.clone());
+        let _ = http_node.data_input.send(data_input_json.clone());
+        http_node.on_update().unwrap();
+
+        mock.assert(); // checks if the mock server has been called
+        let returned_body = mock_output.next().unwrap();
+        assert!(returned_body == expected_response_body, "expected_body: {}, returned_body: {}", expected_response_body, returned_body);
+
+    }
+
+    #[test]
+    fn post_request() {
+        let mut server = mockito::Server::new();
+
+        let url = server.url();
+        let method = "post";
+        let path = "/post_request";
+        let request_body = json!({
+            "name": "Post Doe",
+            "age": 42
+        });
+        let expected_response_body = "Hello Post World!";
+
+        // Create a mock server, which also checks the request body.
+        let mock = server
+            .mock(&method, path.clone())
+            .match_body(serde_json::to_string(&request_body).unwrap().as_str())
+            .with_status(200)
+            .with_body(expected_response_body)
+            .create();
+
+        let change_observer: ChangeObserver = ChangeObserver::new();
+        let data_input_json = json!({
+        "url": url.to_string() + path,
+        "method": method,
+        "body": request_body
+        });
+     
+        let mut http_node: HttpNode = HttpNode::new(Some(&change_observer));
+        let mock_output = Edge::new();
+        connect(http_node.output.clone(), mock_output.clone());
+        let _ = http_node.data_input.send(data_input_json.clone());
+        http_node.on_update().unwrap();
+
+        mock.assert(); // checks if the mock server has been called
+        let returned_body = mock_output.next().unwrap();
+        assert!(returned_body == expected_response_body, "expected_body: {}, returned_body: {}", expected_response_body, returned_body);
+    }
+
+    #[test]
+    fn get_request_with_an_empty_header_value() {
+        let mut server = mockito::Server::new();
+
+        let url = server.url();
+        let method = "GET";
+        let path = "/hello";
+        let content_type_header = ("content-type", "");
+        let x_api_key_header = ("x-api-key", "1234");
+        let expected_response_body = "AI";
+
+        // Create a mock
+        let mock = server
+            .mock(&method, path.clone())
+            .with_status(200)
+            .match_header(&content_type_header.0, content_type_header.1)
+            .match_header(&x_api_key_header.0, x_api_key_header.1)
+            .with_body(&expected_response_body)
+            .create();
+
+
+        let change_observer: ChangeObserver = ChangeObserver::new();
+        let data_input_json = json!({
+        "url": url.to_string() + path,
+        "method": method,
+        "headers": {
+            content_type_header.0: content_type_header.1,
+            x_api_key_header.0: x_api_key_header.1
+        },
+        "body": expected_response_body
+        });
+     
+        let mut http_node: HttpNode = HttpNode::new(Some(&change_observer));
+        let mock_output = Edge::new();
+        connect(http_node.output.clone(), mock_output.clone());
+        let _ = http_node.data_input.send(data_input_json.clone());
+        http_node.on_update().unwrap();
+
+        mock.assert(); // checks if the mock server has been called
+        let returned_body = mock_output.next().unwrap();
+        assert!(returned_body == expected_response_body, "expected_body: {}, returned_body: {}", expected_response_body, returned_body);
+
     }
 }
