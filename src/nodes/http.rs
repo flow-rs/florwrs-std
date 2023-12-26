0e2a0dc000c427708b662d2eba7d508bd54ebb1e
Simon Riepl <s.riepl1@googlemail.com>
2023-12-26 23:25:02 +0100

    doc: add documentation for HttpNode

--- src/nodes/http.rs
+++ src/nodes/http.rs
@@ -12,7 +12,7 @@ use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::Method;
 use serde::{Deserialize, Serialize};
 
-
+/// Own implementation of a HttpMethod enum in order to achieve abstraction of the used HTTP request library.
 #[derive(Clone, Copy, Deserialize)]
 pub enum HTTPMethod {
     GET,
@@ -21,19 +21,26 @@ pub enum HTTPMethod {
     DELETE,
 }
 
+/// Object to specify a HTTP request and is supplied via [`HttpNode::data_input`].
 #[derive(Clone, Deserialize)]
 pub struct RequestInput {
     pub url: String,
     pub method: HTTPMethod,
+    /// Invalid HTTP headers will be ignored and not sent with the request. Also all header names will be converted to lower case.
     pub headers: HashMap<String, String>,
     pub body: Option<String>,
 }
 
+/// Configures [`HttpNode`] for all future requests if supplied once via [`HttpNode::config_input`]. If never supplied, the default configuration is used (see source of [`HttpNode::new()`]).
+#[derive(Clone, Deserialize)]
 pub struct ConfigInput {
+    /// Timeout for all HTTP Requests.
     pub timeout: Option<Duration>,
+    /// Controls if the HTTP client should accept invalid certificates. **Dangerous if set to `true`! Use only in development!**
     pub accept_invalid_certs: Option<bool>,
 }
 
+/// The output object of a executed HTTP request with [`HttpNode::on_update()`].
 #[derive(Clone, Debug)]
 pub struct ResponseOutput {
     pub body: String,
@@ -42,28 +49,37 @@ pub struct ResponseOutput {
     pub content_length: u64,
 }
 
+/// Node which sends HTTP Requests.
 #[derive(RuntimeConnectable, Deserialize, Serialize)]
 pub struct HttpNode {
+    /// Input to execute a single HTTP request via [`Self::on_update()`].
     #[input]
-    pub data_input: Input<RequestInput>, // struct
+    pub data_input: Input<RequestInput>,
 
+    /// Configures [`HttpNode`] for all future requests. If never changed, the default configuration is used (see source of [`Self::new()`]).
     #[input]
-    pub config_input: Input<ConfigInput>, // struct
+    pub config_input: Input<ConfigInput>,
 
+    /// The output of a executed HTTP request with [`Self::on_update()`].
     #[output]
     pub output: Output<ResponseOutput>,
 
+    /// Timeout for all HTTP Requests. This parameter is set via [`Self::config_input`].
     pub timeout: Duration,
+    /// Controls if the HTTP client should accept invalid certificates. **Dangerous! Use only in development!** This parameter is set via [`Self::config_input`].
     pub accept_invalid_certs: bool,
 }
 
 impl HttpNode {
+    /// Creates a new HttpNode instance with the default configuration.
     pub fn new(change_observer: Option<&ChangeObserver>) -> Self {
         Self {
             data_input: Input::new(),
             config_input: Input::new(),
             output: Output::new(change_observer),
+            /// Default timeout configuration.
             timeout: Duration::new(30, 0),
+            /// By default invalid certificates are not accepted.
             accept_invalid_certs: false,
         }
     }
@@ -99,6 +115,57 @@ fn convert_header_map(header_map: &HeaderMap) -> HashMap<String, String> {
 }
 
 impl Node for HttpNode {
+    /// Executes a HTTP request with the values specified in the current [`RequestInput`] object (supplied via [`Self::data_input`]) and the last supplied [`ConfigInput`] object (supplied via [`Self::config_input`]). If no [`ConfigInput`] object was ever supplied, the default values are used (see source of [`Self::new()`]).
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use flowrs::{
+    ///     connection::{connect, Edge},
+    ///     node::{ChangeObserver, Node},
+    /// };
+    /// use flowrs_std::http::{ConfigInput, HTTPMethod, HttpNode, RequestInput};
+    /// use std::{collections::HashMap, time::Duration};
+    ///
+    /// let mut server = mockito::Server::new();
+    /// let url = server.url();
+    /// let method = HTTPMethod::GET;
+    /// let path = "/hello&question=how%20are%20you";
+    /// let expected_response_body = "Hello World!";
+    ///
+    /// // Create a mock
+    /// let mock = server
+    ///     .mock("GET", path.clone())
+    ///     .with_status(200)
+    ///     .with_body(expected_response_body)
+    ///     .create();
+    ///
+    /// let change_observer: ChangeObserver = ChangeObserver::new();
+    /// let data_input = RequestInput {
+    ///     url: url.to_string() + path,
+    ///     method,
+    ///     headers: HashMap::new(),
+    ///     body: None,
+    /// };
+    ///
+    /// let new_timeout = 5000;
+    /// let config_input = ConfigInput {
+    ///     timeout: Some(Duration::from_millis(new_timeout)),
+    ///     accept_invalid_certs: Some(false),
+    /// };
+    ///
+    /// let mut http_node: HttpNode = HttpNode::new(Some(&change_observer));
+    /// let mock_output = Edge::new();
+    /// connect(http_node.output.clone(), mock_output.clone());
+    /// let _ = http_node.config_input.send(config_input);
+    /// let _ = http_node.data_input.send(data_input);
+    /// http_node.on_update().unwrap();
+
+    /// mock.assert(); // checks if the mock server has been called
+    /// let returned_body = mock_output.next().unwrap();
+    /// assert!(returned_body.body == expected_response_body);
+    /// assert!(http_node.timeout == Duration::from_millis(new_timeout));
+    /// ```
     fn on_update(&mut self) -> Result<(), UpdateError> {
         if let Ok(config) = self.config_input.next() {
             if let Some(timeout) = config.timeout {
